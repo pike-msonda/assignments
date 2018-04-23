f0583e1a2e844c1cad70409ea8342927a39dcf74
RenoGunray <pike.msonda@gmail.com>
2018-04-23 22:28:15 +0300

    Added comments to functions

--- data_preprocessing/code.py
+++ data_preprocessing/code.py
@@ -5,24 +5,52 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import entropy
 
-def equal_width(data, info,interval):
+
+#TODO: Add exception handling
+#TODO: Streamline the code to be more efficient 
+
+def equal_width(data, info,bins):
+    '''
+        Basic Binning Method. Splits the attribute into n bins based on intervals. 
+
+        data : pandas.DataFrame
+        info : pandas.DataFrame
+        bins : Integer
+    '''
+
     for column in info:
-        data[column] = pd.cut(data[column], interval,labels=create_label(interval))
+        data[column] = pd.cut(data[column], bins,labels=create_label(bins))
     return data
 
 def remove_classes(data):
+    '''
+        Remove the class column fromt pandas.DataFrame
+
+        data: pandas.DataFrame
+    '''
+
     return data.drop(['Type'], axis = 1)
 
 def create_label(bins):
+    '''
+        Simple Function to create labels correspnding to the numbers of bins requested
+        bins: Integer
+    '''
+
     labels = []
     for x in range(0, bins):
         labels.append("bin"+str(x))
     return labels 
-def probability(X):
-   
-    return X.value_counts(normalize=True, sort=False)
+
 
 def cond_P(A,B):
+    '''
+        Get Probabilities between two attributes. P(A|B)
+
+        A: pandas.DataFrame.column
+        B: pandas.DataFrame.column
+    '''
+
     cond_events = A.groupby(B, sort=False)
     values = cond_events.value_counts(normalize=False).unstack(level=1).fillna(0)
     prob = []
@@ -33,15 +61,34 @@ def cond_P(A,B):
         prob.append([np.divide(r,total), np.divide(m,total)])
     return prob
 
-def entropys(A):
-    total_ent = []
+def get_entropy(A):
+    '''
+        Calculate the entropy of each attribute value
+        
+        A: A list of probabilities. 
+
+    '''
+
+    entropy_list = []  # list to store all the entropy 
     for a in A:
-        if 0 in a:
+        if 0 in a: #Check if the probability has 0, which can't be computed by log2
             ix = a.index(0)
             a[ix] = 1
-        total_ent.append(entropy(a, base=2))
-    return total_ent
+        entropy_list.append(entropy(a, base=2))
+    return entropy_list
+
 def information_gain(data,types):
+    ''' 
+        Function to calcuate the information gain of the given data
+
+        data : given data set. Pandas.DataFrame
+
+        types: data classes.
+
+        Mutual Information of Information Gain: Entropy(S) - Entropy(P|A)
+
+    '''
+    #TODO: Cut down the number of unnecessary operations
     gain = []
     data['Type'] = types
     size= data.index.size
@@ -55,30 +102,32 @@ def information_gain(data,types):
         counts =  data[col].value_counts().values
         counts = [float(x) for x in counts]
         probs = [x / size for x in counts]
-        attribute_entropy = entropys(cond_P(data[col],types))
+        attribute_entropy = get_entropy(cond_P(data[col],types))
         info_gain = total_ent - sum([a * b for a,b in zip(attribute_entropy, probs)])
         gain.append({col: info_gain})
-        
+
     return gain
 
-      
+'''
+    Data Processing Assignment to perform pre-processing operations on a given data set {sonar.dat}
+    Data Description included in the file names.txt
+'''     
 if(__name__ == "__main__"):
-    """
+    '''
     Main program
-    """
+    '''
+
     # Read from sonar.dat file
     sonar_data =  pd.read_table('sonar.dat',sep=',',usecols=[0,1,2,3,60])
     df = remove_classes(sonar_data) # Temporarily remove the type column
     types = sonar_data['Type']
     n = 3
     #5 Row Summary:
-    #print (sonar_data.head())
+    print (sonar_data.head())
     
     # Mean, Mode, Standard Deviation, Variance
-    #print (df.agg(['mean','std','var','min', 'max', 'count',np.percentile(25), ]))
-    # print (sonar_data.quantile([0.25,0.5,0.75]))
-    # # Min, Max Normalisation
-    # # Remove Type column. 
+    print (df.agg(['mean','std','var','min', 'max', 'count','quantile']))
+     
     
     # normalised_data = (df - df.min())/(df.max() -  df.min())
     # print "Data after Min Max Normalisation"
@@ -100,7 +149,6 @@ if(__name__ == "__main__"):
     # ax = sns.swarmplot(data=normalised_data)
 
     #information gain
-    print(df.head())
     eqw= equal_width(df,info, n)
     info_gain = information_gain(eqw, types)
     print ("Information Gain using {} Equal -Width method".format(n))
